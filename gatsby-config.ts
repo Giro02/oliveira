a8c74bccb2680d79697bf0f06d8ae7e8e972a7f8
Bernardo <bernardobremm@gmail.com>
2023-10-05 16:02:02 +0200

    Font correction

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -16,7 +16,20 @@ const config: GatsbyConfig = {
       "path": "./src/images/"
     },
     __key: "images"
-  }]
+  },
+  {
+    resolve: `gatsby-omni-font-loader`,
+    options: {
+      enableListener: true,
+      preconnect: [`https://fonts.googleapis.com`, `https://fonts.gstatic.com`],
+      web: [
+        {
+          name: `Merriweather`,
+          file: `https://fonts.googleapis.com/css2?family=Merriweather:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&display=swap`,
+        },
+      ],
+    },
+  },]
 };
 
 export default config;

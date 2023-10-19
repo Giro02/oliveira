f84f287b5d8e96ee42ef9c0b2a8efdbf9a5081e9
DESKTOP-9NJ6LPQ\pedro <pedroarendt02@hotmail.com>
2023-10-19 15:17:27 -0300

    Updates

---
+++ 0c428ae2-e1acf61a6331ecdbf575.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkoliveira=self.webpackChunkoliveira||[]).push([[13],{3750:function(a,t,r){r.d(t,{Igg:function(){return i}});var e=r(4405);function i(a){return(0,e.w_)({tag:"svg",attr:{fill:"currentColor",viewBox:"0 0 16 16"},child:[{tag:"path",attr:{d:"M8 0a8 8 0 1 1 0 16A8 8 0 0 1 8 0zM4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z"}}]})(a)}}}]);
+//# sourceMappingURL=0c428ae2-e1acf61a6331ecdbf575.js.map
